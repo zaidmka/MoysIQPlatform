62a1a1bf8de3b46181b4730ec5e3034dc2d59e41
zaidmka <zaid.mka.90@gmail.com>
2025-06-28 02:49:21 +0300

    working on test page

---
+++ Client/wwwroot/js/JavaScript.js
@@ -0,0 +1,38 @@
+﻿window.playClickSound = () => {
+    const audio = new Audio('/sounds/click.mp3');
+    audio.play();
+};
+
+let testInstance = null;
+
+window.registerTestInstance = (dotNetObj) => {
+    testInstance = dotNetObj;
+};
+
+window.startTestTimer = function (durationMinutes) {
+    function tryStartTimer() {
+        const display = document.getElementById('test-timer');
+        if (!display) {
+            setTimeout(tryStartTimer, 500);
+            return;
+        }
+
+        let timeLeft = durationMinutes * 60;
+
+        const timerInterval = setInterval(() => {
+            const minutes = Math.floor(timeLeft / 60);
+            const seconds = timeLeft % 60;
+            display.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+
+            if (--timeLeft < 0) {
+                clearInterval(timerInterval);
+                alert("⏰ انتهى الوقت!");
+                if (testInstance) {
+                    testInstance.invokeMethodAsync("SubmitFromTimer");
+                }
+            }
+        }, 1000);
+    }
+
+    tryStartTimer();
+};
---
+++ Client/wwwroot/js/clicksound.js
@@ -0,0 +1,4 @@
+﻿window.playClickSound = () => {
+    const audio = new Audio('/sounds/click.mp3');
+    audio.play();
+};
